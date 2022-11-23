da194c42094c0214b90668f8b2bbc14f156c1244
prislla <priscilaoliveirademattosquinta@gmail.com>
2022-11-23 16:14:56 -0300

    Inicio php 23/11

---
+++ footer.php
@@ -0,0 +1,56 @@
+
+
+  <div class="container-fluid">
+    <div class="row text-center bg-light">
+      <div class="col-md-3">
+        <h4>Institucional</h4>
+        <p><a class="navbar-link" href="#">Home</a></p>
+        <p><a class="navbar-link" href="#">Nossa Academia</a></p>
+        <p><a class="navbar-link" href="#">Conheça os Planos</a></p>
+        <p><a class="navbar-link" href="#">Área do Cliente</a></p>
+      </div>
+
+      <div class="col-md-3">
+        <h4>Atividades</h4>
+        <ul class="navbar-nav mr-auto">
+          <li class="navbar-item"><a class="navbar-link" href="#">Spinning</a></li>
+          <li class="navbar-item"><a class="navbar-link" href="#">Jump</a></li>
+          <li class="navbar-item"><a class="navbar-link" href="#">Hidroginástica</a></li>
+          <li class="navbar-item"><a class="navbar-link" href="#">Pilates</a></li>
+        </ul>
+      </div>
+
+      <div class="col-md-3">
+        <h4>Funcionamento</h4>
+        <p>Segunda à Sábado - 06:00 às 23:00</p>
+        <p>Domingo - 07:00 às 13:00</p>
+      </div>
+
+      <div class="col-md-3 mapa">
+        <h4>Localização</h4>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14701.424874456801!2d-43.5588465!3d-22.9002284!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x588a284ae162bc38!2sSenac%20Campo%20Grande!5e0!3m2!1spt-BR!2sbr!4v1669141608309!5m2!1spt-BR!2sbr" width="200" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+    </div>
+
+      
+ <div class="row text-center">
+        <div class="col-md-12 sociais">
+          <a href="https://pt-br.facebook.com/" target="_blank"><img src="img/face.jpg"></a>
+          <a href="https://www.instagram.com/" target="_blank"><img src="img/insta.jpg"></a>
+          <a href="https://www.whatsapp.com/?lang=pt_br" target="_blank"><img src="img/whats.jpg"></a>
+        </div>
+      </div>
+  </div>
+
+
+  <!-- JavaScript (Opcional) -->
+  <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+    integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+    integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+    crossorigin="anonymous"></script>
---
+++ index.php
@@ -0,0 +1,87 @@
+<?php
+require_once 'head.php';
+require_once 'menu.php';
+?>
+
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <img src="img/academia3.jpg" class="img-fluid">
+      </div>
+
+    </div>
+  </div>
+
+  <div class="container-fluid texto">
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <h2>Conheça Algumas de Nossas Atividades</h2>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid imagens">
+    <div class="row">
+      <div class="col-md-4">
+        <img src="img/academia1.jpg" class="img-fluid">
+      </div>
+
+      <div class="col-md-4">
+        <img src="img/academia2.jpg" class="img-fluid">
+      </div>
+
+      <div class="col-md-4">
+        <img src="img/academia4.webp" class="img-fluid">
+      </div>
+
+    </div>
+  </div>
+
+  <div class="container-fluid text-center conteudo">
+    <div class="row">
+      <div class="col-md-6 col-sm">
+        <img src="img/jumpp.jpg" class="img-fluid">
+      </div>
+      <div class="col-md-6 col-sm">
+        <h3>Jumpp</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora mollitia, nam fugiat illo illum totam sequi laboriosam soluta sed cumque! Officiis dignissimos delectus quae placeat rem magni ut nisi ea!.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora mollitia, nam fugiat illo illum totam sequi laboriosam soluta sed cumque! Officiis dignissimos delectus quae placeat rem magni ut nisi ea!.</p>
+
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid text-center conteudo">
+    <div class="row">
+      <div class="col-md-6 col-sm">
+        <h3>Hidroginástica</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora mollitia, nam fugiat illo illum totam sequi laboriosam soluta sed cumque! Officiis dignissimos delectus quae placeat rem magni ut nisi ea!.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora mollitia, nam fugiat illo illum totam sequi laboriosam soluta sed cumque! Officiis dignissimos delectus quae placeat rem magni ut nisi ea!.</p>
+      </div>
+      <div class="col-md-6 col-sm">
+        <img src="img/hidro.jpg" class="img-fluid">
+
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid text-center conteudo">
+    <div class="row">
+      <div class="col-md-6 col-sm">
+        <img src="img/spinning.jpg" class="img-fluid">
+      </div>
+      <div class="col-md-6 col-sm">
+        <h3>Spinning</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora mollitia, nam fugiat illo illum totam sequi laboriosam soluta sed cumque! Officiis dignissimos delectus quae placeat rem magni ut nisi ea!.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora mollitia, nam fugiat illo illum totam sequi laboriosam soluta sed cumque! Officiis dignissimos delectus quae placeat rem magni ut nisi ea!.</p>
+      </div>
+    </div>
+
+  </div>
+
+  <?php
+      require_once 'footer.php';
+    
+  ?>
+ 
