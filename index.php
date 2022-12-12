2386415f991c489288db1d5df5f0c28052da8867
prislla <priscilaoliveirademattosquinta@gmail.com>
2022-12-12 17:05:01 -0300

    atualizando o site

--- index.php
+++ index.php
@@ -45,9 +45,8 @@ require_once 'menu.php';
       </div>
       <div class="col-md-6 col-sm">
         <h3>Jumpp</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora mollitia, nam fugiat illo illum totam sequi laboriosam soluta sed cumque! Officiis dignissimos delectus quae placeat rem magni ut nisi ea!.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora mollitia, nam fugiat illo illum totam sequi laboriosam soluta sed cumque! Officiis dignissimos delectus quae placeat rem magni ut nisi ea!.</p>
-
+        <p>Para que serve o Jump na academia? Por ser um exercício aeróbico intenso, o Jump vai promover um alto gasto calórico. Em uma aula de cerca de 50 minutos, é possível queimar 600 calorias ou mais, de acordo com a intensidade que os movimentos são praticados.</p>
+        <p>Ele consiste em praticar atividades aeróbicas sobre um pequeno trampolim ou cama elástica, geralmente com o auxílio de música, para dar aquele ânimo. Além dos passos e saltos ritmados, o jump envolve bastante a prática de corridas estáticas, também.</p>
       </div>
     </div>
   </div>
@@ -56,8 +55,8 @@ require_once 'menu.php';
     <div class="row">
       <div class="col-md-6 col-sm">
         <h3>Hidroginástica</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora mollitia, nam fugiat illo illum totam sequi laboriosam soluta sed cumque! Officiis dignissimos delectus quae placeat rem magni ut nisi ea!.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora mollitia, nam fugiat illo illum totam sequi laboriosam soluta sed cumque! Officiis dignissimos delectus quae placeat rem magni ut nisi ea!.</p>
+        <p>A hidroginástica é exatamente isso, são exercícios dentro da água que contam com a possibilidade de realizar exercícios aeróbios ou de força com impacto reduzido sobre as articulações de membro inferiores e coluna.</p>
+        <p>A hidroginástica é uma atividade corporal realizada em um ambiente aquático que proporciona diversos benefícios físicos e para saúde. A hidroginástica é um tipo de atividade física que virou moda na década de 1980 e, desde então, vem ganhando público continuamente.</p>
       </div>
       <div class="col-md-6 col-sm">
         <img src="img/hidro.jpg" class="img-fluid">
@@ -73,8 +72,8 @@ require_once 'menu.php';
       </div>
       <div class="col-md-6 col-sm">
         <h3>Spinning</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora mollitia, nam fugiat illo illum totam sequi laboriosam soluta sed cumque! Officiis dignissimos delectus quae placeat rem magni ut nisi ea!.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora mollitia, nam fugiat illo illum totam sequi laboriosam soluta sed cumque! Officiis dignissimos delectus quae placeat rem magni ut nisi ea!.</p>
+        <p>O spinning, também conhecido como ciclismo indoor, é um exercício aeróbico com mudanças constantes de ritmos feito em uma bicicleta ergométrica. É o exercício físico ideal para quem quer eliminar calorias e definir membros inferiores, deixando pernas mais torneadas e bumbum mais durinho</p>
+        <p>A bicicleta é uma grande aliada para quem busca queimar calorias, melhorar condicionamento físico e ainda definir os músculos de pernas e bumbum. E se você não pode pedalar nas ruas todos os dias, invista no spinning. A aula é feita em uma bicicleta ergométrica parada.</p>
       </div>
     </div>
 
--- spinning.php
+++ spinning.php
@@ -4,50 +4,57 @@
 
 ?>
 
-<div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12 text-center">
-               <h1>Spinning - Conheça seus benefícios</h1>
-            </div>
-
-        </div>
-    </div>
-    
-
-    <div class="container-fluid imagens">
-        <div class="row">
-            <div class="col-md-12">
-                     <img src="img/spinning.jpg" class= "img-fluid">
-                        <!-- Botão para acionar modal -->
-                           <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#impact">
-                            Saiba mais
-                           </button>
-                    </div>
-                </div>
-            </div>
-
-
-            <!-- Modal ATUALIZAR -->
-<div class="modal fade" id="impact" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Conjunto Impact</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <img class="img-fluid" src="img/roupa.jpg" alt="Conjunto Impact">
-        Conjunto acompanha Squeeze, tênis e corda elástica.
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-        <button type="button" class="btn btn-primary">Comprar</button>
+<div class="container-fluid texto">
+    <div class="row">
+      <div class="col-md-12 text-center">
+      <h1>Saiba mais sobre os benefícios do Spinning</h1>
+        <img src="img/spinningcapa.jpg"class="img-fluid">
       </div>
     </div>
   </div>
-</div>
+
+
+  <div class="container-fluid text-center conteudo">
+    <div class="row">
+      <div class="col-md-6 col-sm">
+        <img src="img/spinning1.jpg" class="img-fluid">
+      </div>
+      <div class="col-md-6 col-sm">
+        <h3>Spinning emagrece e Alivia impacto nas articulações e joelhos</h3>
+        <p>Logo de início já é possível garantir que spinning emagrece, por isso, a modalidade é altamente recomendada para quem quer perder peso, tonificar músculos e ganhar resistência e condicionamento.  Em uma aula de cerca de 45 minutos é possível queimar até 600 calorias.</p>
+        <p>Ele consiste em praticar atividades aeróbicas sobre um pequeno trampolim ou cama elástica, geralmente com o auxílio de música, para dar aquele ânimo. Além dos passos e saltos ritmados, o jump envolve bastante a prática de corridas estáticas, também.</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid text-center conteudo">
+    <div class="row">
+      <div class="col-md-6 col-sm">
+        <h3>Hidroginástica</h3>
+        <p>A hidroginástica é exatamente isso, são exercícios dentro da água que contam com a possibilidade de realizar exercícios aeróbios ou de força com impacto reduzido sobre as articulações de membro inferiores e coluna.</p>
+        <p>A hidroginástica é uma atividade corporal realizada em um ambiente aquático que proporciona diversos benefícios físicos e para saúde. A hidroginástica é um tipo de atividade física que virou moda na década de 1980 e, desde então, vem ganhando público continuamente.</p>
+      </div>
+      <div class="col-md-6 col-sm">
+        <img src="img/spinning2.jpg" class="img-fluid">
+
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid text-center conteudo">
+    <div class="row">
+      <div class="col-md-6 col-sm">
+        <img src="img/spinning3.jpg" class="img-fluid">
+      </div>
+      <div class="col-md-6 col-sm">
+        <h3>Spinning</h3>
+        <p>O spinning, também conhecido como ciclismo indoor, é um exercício aeróbico com mudanças constantes de ritmos feito em uma bicicleta ergométrica. É o exercício físico ideal para quem quer eliminar calorias e definir membros inferiores, deixando pernas mais torneadas e bumbum mais durinho</p>
+        <p>A bicicleta é uma grande aliada para quem busca queimar calorias, melhorar condicionamento físico e ainda definir os músculos de pernas e bumbum. E se você não pode pedalar nas ruas todos os dias, invista no spinning. A aula é feita em uma bicicleta ergométrica parada.</p>
+      </div>
+    </div>
+
+  </div>
+
 
 
 
